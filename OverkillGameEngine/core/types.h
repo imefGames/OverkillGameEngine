e7f37ce1020d7e8bc3b0bdd8faba2d1d06d642ae
imefGames <michel.pierre.35@gmail.com>
2018-07-30 00:19:37 +0200

    Added types file
    File contained standard types to be used in engine.

---
+++ OverkillGameEngine/core/types.h
@@ -0,0 +1,19 @@
+
+namespace OK
+{
+    using u64 = unsigned long long;
+    using u32 = unsigned int;
+    using u16 = unsigned short;
+    using u8  = unsigned char;
+
+    using s64 = signed long long;
+    using s32 = signed int;
+    using s16 = signed short;
+    using s8  = signed char;
+
+    using char8  = s8;
+    using char16 = s16;
+
+    using f32 = float;
+    using f64 = double;
+}
