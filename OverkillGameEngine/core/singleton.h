e8451c70919a5a1a806e68bd4881e89b261bbed1
imefGames <michel.pierre.35@gmail.com>
2018-07-31 22:23:04 +0200

    Added CRTP singleton class.

---
+++ OverkillGameEngine/core/singleton.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <core\asserts.h>
+
+namespace OK
+{
+    template <typename SingletonType>
+    class Singleton
+    {
+    public:
+        template <typename ... ConstructorArguments>
+        static void CreateSingleton(ConstructorArguments... arguments)
+        {
+            okAssert(ms_Instance == nullptr, "Singleton has already been created.");
+            ms_Instance = new SingletonType{ arguments... };
+        }
+
+        static void DeleteSingleton()
+        {
+            okAssert(ms_Instance != nullptr, "Singleton does not exist or has already been destroyed.");
+            return ms_Instance;
+        }
+
+        static SingletonType* Get()
+        {
+            okAssert(ms_Instance != nullptr, "Singleton does not exist or has already been destroyed.");
+            return ms_Instance;
+        }
+
+    private:
+        static SingletonType* ms_Instance = nullptr;
+    };
+}
