03743ab09f2f3728f796f189bfe428ffee57b50e
imefGames <michel.pierre.35@gmail.com>
2018-08-12 22:11:54 +0200

    Removed useless includes in main file.

--- OverkillGameEngine/main.cpp
+++ OverkillGameEngine/main.cpp
@@ -3,9 +3,6 @@
 #include <engine\gameengine.h>
 #include <engine\window\gamewindowdata.h>
 
-#include <core\io\parsers\json\jsonparser.h>
-#include <core\io\parsers\json\jsonnode.h>
-
 int CALLBACK WinMain(HINSTANCE instanceHandle, HINSTANCE previousInstanceHandle, LPSTR commandLine, OK::s32 commandCount)
 {
     OK::GameWindowData windowData;
